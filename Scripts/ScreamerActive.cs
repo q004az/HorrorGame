395c42ad784c92008167e5967a6802ac74a5c673
Nazario <71112721+q004az@users.noreply.github.com>
2024-05-20 00:09:04 +0700

    Add files via upload

---
+++ Scripts/ScreamerActive.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreamerActive : MonoBehaviour {
+	public GameObject monstr;
+    
+    void  OnTriggerStay ( Collider other  ){
+        
+        if (other.tag == "Player");
+		monstr.SetActive(true);
+
+      
+
+	}
+}
